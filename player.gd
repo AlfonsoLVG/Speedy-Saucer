550621aa353b5a1aed01701c761ce635135e55f5
AlfonsoLVG <alfonsolvgcontact@gmail.com>
2024-05-27 22:44:22 -0600

    Add player movement WASD

--- player.gd
+++ player.gd
@@ -9,4 +9,12 @@ func _process(delta):
 	pass
 
 func _physics_process(delta):
-	apply_force(Vector2(25, 0))
+	var force = 750
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2(force, 0))
+	if Input.is_action_pressed("move_left"):
+		apply_force(Vector2(-force, 0))
+	if Input.is_action_pressed("move_down"):
+		apply_force(Vector2(0, force))
+	if Input.is_action_pressed("move_up"):
+		apply_force(Vector2(0, -force))
